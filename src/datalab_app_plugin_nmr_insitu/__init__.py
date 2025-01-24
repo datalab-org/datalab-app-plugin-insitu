26eb77777ab244c79fd0fcd80ae0247c717bdfc9
Ben Charmes <107116804+BenjaminCharmes@users.noreply.github.com>
2025-01-24 11:11:43 +0000

    Update __init__.py

--- src/datalab_app_plugin_nmr_insitu/__init__.py
+++ src/datalab_app_plugin_nmr_insitu/__init__.py
@@ -1,6 +1,6 @@
 """Datalab plugin for In Situ NMR"""
 from importlib.metadata import PackageNotFoundError, version
-from .nmr_insitu import process_data
+from .nmr_insitu import process_data, fitting_data
 
 __version__ = version("datalab_app_plugin_nmr_insitu")
-__all__ = ("process_data",)
+__all__ = ("process_data", "fitting_data",)
