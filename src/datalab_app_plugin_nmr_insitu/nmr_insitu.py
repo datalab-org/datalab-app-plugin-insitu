b3d1eb0b07c69073ac77a6f903160c01fe176009
Benjamin CHARMES <benjamin.charmes@gmail.com>
2025-02-05 15:47:23 +0000

    Add pseudo-2D process_data

--- src/datalab_app_plugin_nmr_insitu/nmr_insitu.py
+++ src/datalab_app_plugin_nmr_insitu/nmr_insitu.py
@@ -189,12 +189,14 @@ def process_spectral_data(spec_paths: List[str], time_points: List[float], ppm1:
 def process_pseudo2d_spectral_data(exp_dir: str, ppm1: float, ppm2: float) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Process pseudo-2D spectral data from Bruker files using nmrglue."""
     dic, data = ng.bruker.read(exp_dir)
-    a_dic, a_data = ng.fileio.bruker.read(str(exp_dir))
 
-    print(dic == a_dic)
-    print(data == a_data)
+    p_dic, p_data = ng.fileio.bruker.read_pdata(str(exp_dir))
 
-    pprint.pprint(a_data)
+    print("#%$#$#$%#$#%$#%$#")
+    print(p_dic)
+    print("#%$#$#$%#$#%$#%$#")
+    print(p_data)
+    print("#%$#$#$%#$#%$#%$#")
 
     td = int(dic['acqus'].get('TD', 0)) // 2
     td_indirect = int(dic['acqu2s'].get('TD', 0))
