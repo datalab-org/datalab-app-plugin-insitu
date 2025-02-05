f7365e1134bc4fc0cc7f90a20302ffd3ba435116
Benjamin CHARMES <benjamin.charmes@gmail.com>
2025-02-05 11:38:11 +0000

    Add pseudo-2D process_data

--- src/datalab_app_plugin_nmr_insitu/nmr_insitu.py
+++ src/datalab_app_plugin_nmr_insitu/nmr_insitu.py
@@ -39,7 +39,13 @@ def check_nmr_dimension(nmr_folder_path: str) -> str:
             raise FileNotFoundError("No experiment folders found in NMR data")
 
         if len(exp_folders) == 1:
-            exp_path = os.path.join(nmr_folder_path, exp_folders[0])
+            exp_folders = [d for d in os.listdir(nmr_folder_path) if os.path.isdir(
+                os.path.join(nmr_folder_path, d)) and d.isdigit()]
+            if exp_folders:
+                exp_folder = os.path.join(nmr_folder_path, exp_folders[0])
+            else:
+                raise FileNotFoundError(
+                    "No experiment folders found in NMR data")
             acqus_path = os.path.join(exp_path, "acqus")
             acqu2s_path = os.path.join(exp_path, "acqu2s")
 
