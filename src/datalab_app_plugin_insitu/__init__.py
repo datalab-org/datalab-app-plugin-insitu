ff0462c3ce2be880129c725d24ef2861216d076a
BenSmithGreyGroup <ben.ed.smith@mac.com>
2025-06-10 15:02:25 +0100

    Added UVVisblock to plugins in pyproject.toml and __init__.py

--- src/datalab_app_plugin_insitu/__init__.py
+++ src/datalab_app_plugin_insitu/__init__.py
@@ -2,9 +2,9 @@
 
 from importlib.metadata import version
 
-from .blocks import InsituBlock
+from .blocks import InsituBlock, UVVisInsituBlock
 from .nmr_insitu import process_datalab_data, process_local_data
 
 __version__ = version("datalab-app-plugin-insitu")
 
-__all__ = ("__version__", "process_local_data", "process_datalab_data", "InsituBlock")
+__all__ = ("__version__", "process_local_data", "process_datalab_data", "InsituBlock", "UVVisInsituBlock")
