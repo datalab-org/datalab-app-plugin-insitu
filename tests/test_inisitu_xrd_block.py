eef047898db24e04f657df510ed15c3a01f8b1c8
BenSmithGreyGroup <ben.ed.smith@mac.com>
2025-10-30 14:08:24 +0000

    Added test for echem pathway and example data

--- tests/test_inisitu_xrd_block.py
+++ tests/test_inisitu_xrd_block.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from datalab_api.utils import bokeh_from_json
 
 
-def test_block(pytestconfig):
+def test_block_temperature_mode(pytestconfig):
     from datalab_app_plugin_insitu.apps.xrd.blocks import XRDInsituBlock
 
     test_data_zip = (
@@ -13,6 +13,25 @@ def test_block(pytestconfig):
     block.data["time_series_source"] = "log"
     block.data["xrd_folder_name"] = "43_up"
     block.data["time_series_folder_name"] = "log"
+    block.generate_insitu_xrd_plot(file_path=test_data_zip, link_plots=True)
+
+    assert block.data["bokeh_plot_data"] is not None
+
+    if pytestconfig.getoption("show_plots"):
+        bokeh_from_json(block.data["bokeh_plot_data"])
+
+
+def test_block_echem_mode(pytestconfig):
+    from datalab_app_plugin_insitu.apps.xrd.blocks import XRDInsituBlock
+
+    test_data_zip = (
+        Path(__file__).parent.parent / "example_data" / "xrd" / "coincell8_LDE2_reduced.zip"
+    )
+    block = XRDInsituBlock(item_id="test-xrd-insitu-echem")
+    block.data["time_series_source"] = "echem"
+    block.data["xrd_folder_name"] = "xrd_data"
+    block.data["time_series_folder_name"] = "Log"
+    block.data["echem_folder_name"] = "Echem"
     block.generate_insitu_xrd_plot(file_path=test_data_zip, link_plots=True)
 
     assert block.data["bokeh_plot_data"] is not None
